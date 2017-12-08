4104a447227da3cf0a9a2c95ede5cf4201d2d96f
Tamas Stanitz <tamas.stanitz@gmail.com>
2017-12-08 11:18:27 +0100

    C# server and client

---
+++ GrpcClient/Program.cs
@@ -0,0 +1,23 @@
+﻿using Grpc.Core;
+using System;
+using System.Threading.Tasks;
+using static GrpcDefinition.SalutationServer;
+
+namespace GrpcClient
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var channel = new Channel("127.0.0.1:50050", ChannelCredentials.Insecure);
+
+            var client = new SalutationServerClient(channel);
+
+            var reply = await client.SaluteAsync(new SampleRequest { Name = "C#" });
+
+            Console.WriteLine(reply.Salutation);
+
+            await channel.ShutdownAsync();
+        }
+    }
+}
---
+++ GrpcServer/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Threading.Tasks;
+using Grpc.Core;
+using static GrpcDefinition.SalutationServer;
+
+namespace GrpcServer
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var server = new Server
+            {
+                Ports = { new ServerPort("localhost", 50050, ServerCredentials.Insecure) },
+                Services = { BindService(new SalutationServerImpl()) }
+            };
+            server.Start();
+            Console.ReadKey();
+            await server.ShutdownAsync();
+        }
+    }
+
+    public class SalutationServerImpl : SalutationServerBase
+    {
+        public override Task<SampleResponse> Salute(SampleRequest request, ServerCallContext context)
+        {
+            return Task.FromResult(new SampleResponse { Salutation = $"Salute {request.Name} from Server" });
+        }
+    }
+}
